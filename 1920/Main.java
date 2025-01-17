2bbe9da4fdb6ded853cc9a9b2712d364cf65e81a
Jeonghyeon Park <jpark634@wisc.edu>
2025-01-17 21:33:10 +0900

    1920

--- 1920/Main.java
+++ 1920/Main.java
@@ -3,42 +3,48 @@ import java.io.*;
 
 public class Main {
     static int N, M;
-    static int[] nArr, mArr;
+    static int[] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        Set<Integer> set = new HashSet<>();
+        arr = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        Arrays.sort(arr);
         StringBuilder sb = new StringBuilder();
+
         M = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < M; i++) {
-            if (set.contains(Integer.parseInt(st.nextToken()))) {
-                sb.append(1).append("\n");
+            int num = Integer.parseInt(st.nextToken());
+
+            int left = 0;
+            int right = N - 1;
+            boolean flag = false;
+
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                if (arr[mid] == num) {
+                    flag = true;
+                    sb.append(1).append("\n");
+                    break;
+                }
+
+                else if (arr[mid] < num) {
+                    left = mid + 1;
+                }
+
+                else {
+                    right = mid - 1;
+                }
             }
-            else sb.append(0).append("\n");
+
+            if (!flag) sb.append(0).append("\n");
         }
-
         System.out.println(sb.toString());
-        // nArr = new int[N];
-
-        // StringTokenizer st = new StringTokenizer(br.readLine());
-        // for (int i = 0; i < N; i++) {
-        //     nArr[i] = Integer.parseInt(st.nextToken());
-        // }
-
-        // M = Integer.parseInt(br.readLine());
-        // mArr = new int[M];
-        // st = new StringTokenizer(br.readLine());
-        // for (int i = 0; i < M; i++) {
-        //     mArr[i] = Integer.parseInt(st.nextToken());
-        // }
-
-        // StringBuilder sb = new StringBuilder();
     }
 }
 
