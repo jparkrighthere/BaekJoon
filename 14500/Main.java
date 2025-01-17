ed8d5deb2daa07dfbfeca1d3e8107510185d1185
Jeonghyeon Park <jpark634@wisc.edu>
2025-01-17 17:06:15 +0900

    14500

--- 14500/Main.java
+++ 14500/Main.java
@@ -3,16 +3,43 @@ import java.io.*;
 
 public class Main {
     static int N, M;
-    static int[][] map;
-    static int MAX = Integer.MIN_VALUE;
-    static boolean[][] visited;
-    static int[] dy = {-1, 1, 0, 0};
+    static int[] dy = {-1 ,1 , 0, 0};
     static int[] dx = {0, 0, -1, 1};
-    static List<int[]> list;
+    static int[][] map;
+    static boolean[][] visited;
+    static int ans = Integer.MIN_VALUE;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-    static void dfs(int y, int x, int depth, int sum, List<int[]> store) {
-        if (depth == 3) {
-            combi(sum, store);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (!visited[i][j]) {
+                    visited[i][j] = true;
+                    List<int[]> list = new ArrayList<>();
+                    list.add(new int[]{i, j});
+                    dfs(i, j, map[i][j], list);
+                    visited[i][j] = false;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+
+    static void dfs(int y, int x, int sum, List<int[]> list) {
+        if (list.size() == 3) {
+            int totalSum = createCombi(sum, list);
+            ans = Math.max(ans, totalSum);
             return;
         }
 
@@ -22,57 +49,27 @@ public class Main {
 
             if (ny >= 0 && ny < N && nx >= 0 && nx < M && !visited[ny][nx]) {
                 visited[ny][nx] = true;
-                store.add(new int[]{ny, nx});
-                dfs(ny, nx, depth + 1, sum + map[ny][nx], store);
+                list.add(new int[]{ny, nx});
+                dfs(ny, nx, sum + map[ny][nx], list);
                 visited[ny][nx] = false;
-                store.remove(store.size() - 1);
+                list.remove(list.size() - 1);
             }
         }
     }
 
-    static void combi(int sum, List<int[]> store) {
-        for (int i = 0; i < store.size(); i++) {
-            int cy = store.get(i)[0];
-            int cx = store.get(i)[1];
-            for (int j = 0; j < 4; j++) {
-                int ny = cy + dy[j];
-                int nx = cx + dx[j];
+    static int createCombi(int sum, List<int[]> list) {
+        int max = sum;
+        for (int[] pos : list) {
+            for (int i = 0; i < 4; i++) {
+                int ny = pos[0] + dy[i];
+                int nx = pos[1] + dx[i];
+
                 if (ny >= 0 && ny < N && nx >= 0 && nx < M && !visited[ny][nx]) {
-                    int tempSum = sum + map[ny][nx];
-                    MAX = Math.max(MAX, tempSum);
+                    int newSum = sum + map[ny][nx];
+                    if (newSum > max) max = newSum;
                 }
             }
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
-        visited = new boolean[N][M];
-
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-        list = new ArrayList<>();
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (!visited[i][j]) {
-                    visited[i][j] = true;
-                    List<int[]> store = new ArrayList<>();
-                    store.add(new int[]{i, j});
-                    dfs(i, j, 1, map[i][j], store);
-                    visited[i][j] = false;
-                }
-            }
-        }
-
-        System.out.println(MAX);
+        return max;
     }
 }
